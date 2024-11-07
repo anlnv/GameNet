5e954159c61314e198f4dacc6fa656b7b1bc67d9
Luneva <aaluneva@edu.hse.ru>
2024-11-08 01:28:00 +0300

    profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Profile />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import logo from '../images/logo.svg';
 
-
-
 function Header() {
     return (
         <header className="header">
             <img className="header__logo" alt="header__logo" src={logo} />
             <p className="header__name">GameNet</p>
+            <div className="header__links">
+                <a href="/" className="header__link">Home</a>
+                <a href="/" className="header__link">Discover</a>
+                <a href="/" className="header__link">Esports</a>
+                <a href="/" className="header__link">Game Events</a>
+            </div>
+            <nav className="navbar">
+                <button className="navbar__icon navbar__icon_search"></button>
+                <button className="navbar__icon navbar__icon_notifications"></button>
+                <button className="navbar__icon navbar__icon_profile"></button>
+            </nav>
         </header>
     )
 }
